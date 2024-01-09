33e64b2c34a666a7ab79bb65b8011d754bd77895
LinThit27 <linthit2745@gmail.com>
2024-01-10 00:15:29 +0630

    [add] add custom font and navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <nav className="flex h-14 items-center justify-between p-3 md:p-5">
+      <h1 className="text-primary/90 font-koblenz  text-4xl font-extrabold tracking-wide">
+        ASE
+      </h1>
+      <ul className="mr-6 flex w-auto gap-8 text-base font-[400]">
+        <li>
+          <span>Home</span>
+        </li>
+        <li>
+          <span>About Us</span>
+        </li>
+        <li>
+          <span>Services</span>
+        </li>
+        <li>
+          <span>Contact Us</span>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
